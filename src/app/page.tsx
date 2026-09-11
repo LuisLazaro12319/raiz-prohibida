24ce72b7abaa023178a9bf53a1a88fd9eb1d7947
LuisLazaro12319 <luislazaro12319@gmail.com>
2026-09-10 21:47:16 -0300

    Hero: agrega el mismo brillo rosado que la seccion mayorista

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,6 +74,7 @@ export default function Home() {
             priority
             className="object-cover object-top"
           />
+          <div className="absolute -right-16 top-1/4 h-[380px] w-[380px] rounded-full bg-acento/20 blur-[110px]" />
           <div className="absolute inset-0 bg-gradient-to-b from-background/85 via-background/65 to-background/90" />
         </div>
 
@@ -86,6 +87,7 @@ export default function Home() {
             priority
             className="object-cover object-top"
           />
+          <div className="absolute -right-24 top-1/3 h-[500px] w-[500px] rounded-full bg-acento/20 blur-[140px]" />
           <div className="absolute inset-y-0 left-0 w-1/2 bg-gradient-to-r from-background via-background/60 to-transparent" />
         </div>
 
